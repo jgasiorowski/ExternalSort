ccab3cc982fd1fe07c96c9a8d9cad9d702da0677
Jacek Gasiorowski <gasiorowski.j@gmail.com>
2025-02-24 22:36:25 +0100

    Initial file generator

---
+++ ExternalSort/Generator.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+
+namespace Generator.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker("en");
+            var path = "f:/temp/1gb";
+
+            var size = 1024 * 1024 * 1024;
+            using (var file = new StreamWriter(path)) 
+            {
+                do
+                {
+                    file.WriteLine($"{faker.Random.Long(1)}. {faker.Random.Words()}");
+
+                } while (file.BaseStream.Length < size);
+
+                file.Flush();
+                file.Close();
+            }
+
+        }
+    }
+}
